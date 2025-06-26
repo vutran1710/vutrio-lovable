3aa443d8cdb4e0d00279d469becd808939ace837
vutran1710 <me@vutr.io>
2025-06-26 10:22:14 +0700

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { EB_Garamond, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const ebgaramon = EB_Garamond({
+  variable: "--font-sans",
+  subsets: ["latin", "vietnamese"],
+  weight: ["400", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ibmplexmono = IBM_Plex_Mono({
+  variable: "--font-mono",
+  subsets: ["latin", "vietnamese"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ebgaramon.variable} ${ibmplexmono.variable} antialiased`}
       >
         <div id="root">{children}</div>
       </body>
