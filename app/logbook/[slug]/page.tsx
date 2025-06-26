8db1456bd684ba1882d4fce285b3493b9e666201
vutran1710 <me@vutr.io>
2025-06-26 11:22:50 +0700

    Update logbook page

--- app/logbook/[slug]/page.tsx
+++ app/logbook/[slug]/page.tsx
@@ -1,10 +1,23 @@
+import { getLogbookEntryBySlug } from "@/lib/notion";
 import { Footer, Header, PageContainer, LogbookPostBody } from "@/ui";
+import { notFound } from "next/navigation";
 
-export default function LogbookPostPage() {
+export default async function LogbookPostPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = await params;
+  const post = await getLogbookEntryBySlug(slug);
+
+  if (!post) {
+    return notFound();
+  }
+
   return (
     <PageContainer>
       <Header currentPath="/logbook" />
-      <LogbookPostBody />
+      <LogbookPostBody post={post} />
       <Footer />
     </PageContainer>
   );
