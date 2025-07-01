80b6b18c4133f319a039b19d6648177f0ebe6482
vutran1710 <me@vutr.io>
2025-07-01 20:56:17 +0700

    update

---
+++ app/logbook/tag/[tag]/page.tsx
@@ -0,0 +1,7 @@
+export default function LogbookTag() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">Logbook Tag Page</h1>
+    </div>
+  );
+}
---
+++ app/search/page.tsx
@@ -0,0 +1,7 @@
+export default function Search() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Search is not implemented yet</h1>
+    </div>
+  );
+}
